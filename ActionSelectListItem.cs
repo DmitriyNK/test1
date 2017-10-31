b87189812fdc2cca834cae553af387b411e1045d
Dmitriy Kozhemyakin <dnk@intravision.ru>
2017-10-31 12:59:48 +0300

    add file

---
+++ ActionSelectListItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace IntraCoffee.Domain.Mvc
+{
+    /// <summary>
+    /// Представляет выбранный элемент в экземпляре класса <see cref="T:dmp.Domain.Mvc.ActionSelectListItem"/>.
+    /// </summary>
+    public class ActionSelectListItem : SelectListItem
+    {
+        /// <summary>
+        /// Получает или задает аттрибуты выбранного элемента.
+        /// </summary>
+        /// <returns>
+        /// Аттрибуты.
+        /// </returns>
+        public object Attributes { get; set; }
+
+        public string Indent { get; set; }
+    } 
+}
