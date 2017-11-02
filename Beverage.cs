ed6cd37677d7b7a43ee9513ce4b823c39846638f
Dmitriy Kozhemyakin <dnk@intravision.ru>
2017-11-02 12:46:53 +0300

    2 new files

---
+++ Beverage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace IntraCoffee.Domain.Model
+{
+    /// <summary>
+    /// Напиток
+    /// </summary>
+    public  class Beverage : IEntity
+    {
+        private ICollection<BeveragePrice> _beveragePrices;
+
+        public Beverage()
+        {
+            _beveragePrices = new HashSet<BeveragePrice>();
+        }
+
+        [Key]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<BeveragePrice> BeveragePrices
+        {
+            get { return _beveragePrices; }
+            set { _beveragePrices = value; }
+        }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public virtual byte[] Image { get; set; }
+
+        [Required]
+        public virtual byte[] ThumbnailImage { get; set; }
+
+        [Required]
+        public string Composition { get; set; }
+
+        [Required]
+        public bool IsArchive { get; set; }
+    }
+}
---
+++ BeverageOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntraCoffee.Domain.Model
+{
+    public class BeverageOrder:IEntity
+    {
+        private ICollection<Condiment> _condiments;
+
+        public BeverageOrder()
+        {
+            _condiments=new HashSet<Condiment>();
+        }
+
+        [Key]
+        public long Id { get; set; }
+
+        public long BeveragePriceId { get; set; }
+
+        [ForeignKey("BeveragePriceId"), Required]
+        public virtual BeveragePrice BeveragePrice { get; set; }
+        
+        [ForeignKey("OrderId"), Required]
+        public virtual Order Order { get; set; }
+
+        public long OrderId { get; set; }
+
+        public virtual ICollection<Condiment> Condiments
+        {
+            get { return _condiments; }
+            set { _condiments = value; }
+        }
+    }
+}
